4ece04968c66d759e160e3493950d855f6d5c828
yuki678 <uk.12juni@gmail.com>
2020-06-12 16:34:11 +0100

    Fix: Add special handling for specific meeting dates

--- src/FomcGetCalendar.py
+++ src/FomcGetCalendar.py
@@ -48,6 +48,12 @@ if __name__ == '__main__':
 
     date_list = []
     pg_name = sys.argv[0]
+
+    if len(sys.argv) != 2:
+        print("Usage: ", pg_name)
+        print("Please specify the first argument between 1936 and 2015")
+        sys.exit(1)    
+        
     from_year = sys.argv[1]
 
     # Handles the first argument, from_year
@@ -76,6 +82,7 @@ if __name__ == '__main__':
         m_year = panel_div.find('h4').get_text()[:4]
         m_months = panel_div.find_all('div', {"class": "fomc-meeting__month"})
         m_dates = panel_div.find_all('div', {"class": "fomc-meeting__date"})
+        print("YEAR: {} - {} meetings found.".format(m_year, len(m_dates)))
 
         for (m_month, m_date) in zip(m_months, m_dates):
             month_name = m_month.get_text().strip()
@@ -84,8 +91,10 @@ if __name__ == '__main__':
             is_unscheduled = False
             is_month_short = False
 
-            if ("cancelled" in date_text) or ("notation vote" in date_text):
+            if ("cancelled" in date_text):
                 continue
+            elif "notation vote" in date_text:
+                date_text = date_text.replace("(notation vote)", "").strip()
             elif "unscheduled" in date_text:
                 date_text = date_text.replace("(unscheduled)", "").strip()
                 is_unscheduled = True
@@ -110,22 +119,33 @@ if __name__ == '__main__':
             date_list.append({"date": meeting_date, "unscheduled": is_unscheduled, "forecast": is_forecast, "confcall": False})
 
     # Retrieve FOMC Meeting date older than 2015
-    for year in tqdm(range(from_year, 2015)):
+    for year in range(from_year, 2015):
         hist_url = base_url + '/monetarypolicy/fomchistorical' + str(year) + '.htm'
         r = requests.get(hist_url)
         soup = BeautifulSoup(r.text, 'html.parser')
-        panel_headings = soup.find_all('div', {"class": "panel-heading"})
+        if year in (2011, 2012, 2013, 2014):
+            panel_headings = soup.find_all('h5', {"class": "panel-heading"})
+        else:
+            panel_headings = soup.find_all('div', {"class": "panel-heading"})
+        print("YEAR: {} - {} meetings found.".format(year, len(panel_headings)))
         for panel_heading in panel_headings:
             date_text = panel_heading.get_text().strip()
-            #print(date_text)
-            regex = r"(January|February|March|April|May|June|July|August|September|October|November|December).*\s(\d*-)*(\d+)\s+(Meeting|Conference Calls?)\s-\s(\d+)"
+            #print("Date: ", date_text)
+            regex = r"(January|February|March|April|May|June|July|August|September|October|November|December).*\s(\d*-)*(\d+)\s+(Meeting|Conference Calls?|\(unscheduled\))\s-\s(\d+)"
             date_text_ext = re.findall(regex, date_text)[0]
-            #print(date_text_ext)
-
             meeting_date_str = date_text_ext[4] + "-" + date_text_ext[0] + "-" + date_text_ext[2]
+            #print("   Extracted:", meeting_date_str)
+            if meeting_date_str == '1995-January-1':
+                meeting_date_str = '1995-February-1'
+            elif meeting_date_str == '2012-July-1':
+                meeting_date_str = '2012-August-1'
+            elif meeting_date_str == '2013-April-1':
+                meeting_date_str = '2013-May-1'
+
             meeting_date = datetime.strptime(meeting_date_str, '%Y-%B-%d')
             is_confcall = "Conference Call" in date_text_ext[3]
-            date_list.append({"date": meeting_date, "unscheduled": False, "forecast": False, "confcall": is_confcall})
+            is_unscheduled = "unscheduled" in date_text_ext[3]
+            date_list.append({"date": meeting_date, "unscheduled": is_unscheduled, "forecast": False, "confcall": is_confcall})
 
     df = pd.DataFrame(date_list).sort_values(by=['date'])
     df.reset_index(drop=True, inplace=True)
